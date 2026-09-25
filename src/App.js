997fe13e3f3e236bc1422af44126be5a2f7baaac
Amreen-0786 <amreenshaikh6303@gmail.com>
2026-09-25 15:01:58 +0530

    firsr commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Ex2a_CounterClass from './Ex2a_CounterClass';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return <Ex2a_CounterClass />;
 }
-
 export default App;
